ce5c72605ccebf8ff9245eafed420995106aeb9a
Zach Hirsch <zhirsch@umich.edu>
2014-09-05 15:30:23 -0700

    Better implementation of isatty.

--- kernel/src/syscall/isatty.c
+++ kernel/src/syscall/isatty.c
@@ -1,6 +1,12 @@
 #include "syscalls/syscalls.h"
 
+#include "bits/errno.h"
+
+#include "process.h"
+
 int sys_isatty(int fd) {
-  // TODO(zhirsch): Better implementation...
-  return (fd == 0 || fd == 1 || fd == 2);
+  if (fd < 0 || fd > NUM_FDS) {
+    return -EBADF;
+  }
+  return (current_process->fds[fd].type == PROCESS_FD_TTY);
 }
