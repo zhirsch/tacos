eb05fa4fc3e99f04a67628a5a08076558f74e5a7
Zachary Hirsch <zhirsch@umich.edu>
2015-12-27 19:25:53 -0500

    More complete implementation of _exit.
    Marks the process as a zombie and schedules something else.

--- kernel/src/syscall/_exit.c
+++ kernel/src/syscall/_exit.c
@@ -2,9 +2,23 @@
 
 #include "interrupts.h"
 #include "log.h"
+#include "process.h"
+#include "scheduler.h"
 
+#define LOG(...) log("SYSCALL [_exit]", __VA_ARGS__)
 #define PANIC(...) panic("SYSCALL [_exit]", __VA_ARGS__)
 
 void sys__exit(int status, struct isr_frame* frame) {
-  PANIC("Process terminated with status %d\n", status);
+  LOG("pid %d terminated with status %d\n", current_process->pid, status);
+
+  // Set the process as a zombie.
+  current_process->state = PROCESS_ZOMBIE;
+
+  // Allow waitpid to work for this process.
+  current_process->wait_state = PROCESS_WAIT_EXITED;
+  current_process->status = status;
+
+  // Yield to something else, this process is dead.
+  scheduler_yield();
+  PANIC("scheduler_yield returned to _exit\n");
 }
